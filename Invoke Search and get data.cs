12016469594244653782947fc597da12bdf8b6c6
Evan.Mateo <gevanmateo@gmail.com>
2017-10-23 08:52:53 -0400

    Init

---
+++ Invoke Search and get data.cs
@@ -0,0 +1,18 @@
+
+this.custAdapter = new CustomerAdapter(this.oTrans);
+this.custAdapter.BOConnect();
+
+// Prepare single Where clause
+    string whereClause = "CustID = \'" + custID + custSuffix.ToString("D3") + "\'";
+
+// Set Hashtable Where clauses
+    System.Collections.Hashtable whereClauses = new System.Collections.Hashtable(1);
+    whereClauses.Add("Customer", whereClause);
+
+// Invoke Search with Hashtable Where clauses
+    SearchOptions searchOptions = SearchOptions.CreateRuntimeSearch(whereClauses, DataSetMode.RowsDataSet);
+    this.custAdapter.InvokeSearch(searchOptions);
+    
+// Get Data Row
+DataRow dataRow = this.adapterName.TableNameData.TableName.Rows[row index];
+dataRow["Column Name"]
