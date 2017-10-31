91c3db6624779f9dc3ebbfe7cd8edb111a51c183
Evan.Mateo <gevanmateo@gmail.com>
2017-10-31 11:15:43 -0400

    Add Call type and Points logic
    Points is enabled only if call type is call

--- CRM Call Log - Conditional Required Fields.cs
+++ CRM Call Log - Conditional Required Fields.cs
@@ -27,11 +27,13 @@ public class Script
 	// Begin Wizard Added Module Level Variables **
 
 	private EpiBaseAdapter oTrans_adapter;
+	private EpiDataView edvCRMCall;
 	// End Wizard Added Module Level Variables **
 
 	// Add Custom Module Level Variables Here **
 	private static EpiCombo callType;
 	private static EpiCombo inquiryType;
+	private static EpiCombo points;
 	public void InitializeCustomCode()
 	{
 		// ** Wizard Insert Location - Do not delete 'Begin/End Wizard Added Variable Initialization' lines **
@@ -39,6 +41,12 @@ public class Script
 
 		this.oTrans_adapter = ((EpiBaseAdapter)(this.csm.TransAdaptersHT["oTrans_adapter"]));
 		this.oTrans_adapter.BeforeAdapterMethod += new BeforeAdapterMethod(this.oTrans_adapter_BeforeAdapterMethod);
+		this.edvCRMCall = ((EpiDataView)(this.oTrans.EpiDataViews["CRMCall"]));
+		this.edvCRMCall.EpiViewNotification += new EpiViewNotification(this.edvCRMCall_EpiViewNotification);
+
+		callType = ((EpiCombo)csm.GetNativeControlReference("aa40b07c-65e2-4cec-bc2a-1fc794aee8cb"));
+		inquiryType = ((EpiCombo)csm.GetNativeControlReference("25e32f3c-cd04-4c18-8674-3b58b309078f"));
+		points = ((EpiCombo)csm.GetNativeControlReference("f16c1292-c473-4962-b4ae-940542c38ea0"));
 		// End Wizard Added Variable Initialization
 
 		// Begin Wizard Added Custom Method Calls
@@ -53,6 +61,12 @@ public class Script
 
 		this.oTrans_adapter.BeforeAdapterMethod -= new BeforeAdapterMethod(this.oTrans_adapter_BeforeAdapterMethod);
 		this.oTrans_adapter = null;
+		this.edvCRMCall.EpiViewNotification -= new EpiViewNotification(this.edvCRMCall_EpiViewNotification);
+		this.edvCRMCall = null;
+
+		callType = null;
+		inquiryType = null;
+		points = null;
 		// End Wizard Added Object Disposal
 
 		// Begin Custom Code Disposal
@@ -65,7 +79,7 @@ public class Script
 		// ** Argument Properties and Uses **
 		// ** args.MethodName **
 		// ** Add Event Handler Code **
-
+	
 		// ** Use MessageBox to find adapter method name
 		// EpiMessageBox.Show(args.MethodName)
 		switch (args.MethodName)
@@ -79,14 +93,36 @@ public class Script
 				// {
 				// 	DoSomethingElse();
 				// }
-				callType = ((EpiCombo)csm.GetNativeControlReference("aa40b07c-65e2-4cec-bc2a-1fc794aee8cb"));
-				inquiryType = ((EpiCombo)csm.GetNativeControlReference("25e32f3c-cd04-4c18-8674-3b58b309078f"));
+				
 				if(callType.Text.ToLower().Contains("sale") && inquiryType.Text.Trim().Length == 0)
 				{
 					throw new UIException("Inquiry Type must be filled for new sales.");
 				}
 				break;
 		}
+	
+	}
 
+	private void edvCRMCall_EpiViewNotification(EpiDataView view, EpiNotifyArgs args)
+	{
+		// ** Argument Properties and Uses **
+		// view.dataView[args.Row]["FieldName"]
+		// args.Row, args.Column, args.Sender, args.NotifyType
+		// NotifyType.Initialize, NotifyType.AddRow, NotifyType.DeleteRow, NotifyType.InitLastView, NotifyType.InitAndResetTreeNodes
+		
+		if ((args.NotifyType == EpiTransaction.NotifyType.Initialize))
+		{
+			if(edvCRMCall.CurrentDataRow != null){
+				if(edvCRMCall.CurrentDataRow["CallTypeCode"].ToString().ToLower().Contains("call")) {
+					if(points != null) points.Enabled = true;
+				}
+				else {
+					if(points != null){
+						edvCRMCall.CurrentDataRow["Points_c"] = "0";
+						points.Enabled = false;
+					}
+				}
+			}
+		}
 	}
 }
