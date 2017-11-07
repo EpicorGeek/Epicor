2e422b0678363ab9864828ca81f04822f64ac788
Evan.Mateo <gevanmateo@gmail.com>
2017-11-07 07:02:31 -0500

    Add Simple Search Function

--- Invoke Search and get data.cs
+++ Invoke Search and get data.cs
@@ -17,3 +17,25 @@ this.custAdapter.BOConnect();
 // Get Data Row
     DataRow dataRow = this.adapterName.TableNameData.TableName.Rows[row index];
     dataRow["Column Name"]
+    
+
+// Simple Search Function List Loopup
+    bool recSelected;
+    string whereClause = string.Empty;
+    System.Data.DataSet dsCustomerAdapter = Ice.UI.FormFunctions.SearchFunctions.listLookup(this.oTrans, "CustomerAdapter", out recSelected, true, whereClause);
+    if (recSelected)
+    {
+        System.Data.DataRow adapterRow = dsCustomerAdapter.Tables[0].Rows[0];
+
+        // Map Search Fields to Application Fields
+        EpiDataView edvCustomer = ((EpiDataView)(this.oTrans.EpiDataViews["Customer"]));
+        System.Data.DataRow edvCustomerRow = edvCustomer.CurrentDataRow;
+        if ((edvCustomerRow != null))
+        {
+            edvCustomerRow.BeginEdit();
+            edvCustomerRow["ParentCustNum_c"] = adapterRow["CustNum"];
+            edvCustomerRow["ParentCustID_c"] = adapterRow["CustID"];
+            edvCustomerRow.EndEdit();
+            
+        }
+    }
