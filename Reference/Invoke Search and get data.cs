70524dec8385e485c170e0341ce70cc015b4d8cf
Evan.Mateo <gevanmateo@gmail.com>
2017-12-22 07:03:52 -0500

    Update references
    Add getter for data

--- Reference/Invoke Search and get data.cs
+++ Reference/Invoke Search and get data.cs
@@ -4,31 +4,50 @@
 this.custAdapter = new CustomerAdapter(this.oTrans);
 this.custAdapter.BOConnect();
 
+
 // Prepare single Where clause
-    string whereClause = "CustID = \'" + custID + custSuffix.ToString("D3") + "\'";
+string whereClause = "CustID = \'" + custID + custSuffix.ToString("D3") + "\'";
 
 // Set Hashtable Where clauses
-    System.Collections.Hashtable whereClauses = new System.Collections.Hashtable(1);
-    whereClauses.Add("Customer", whereClause);
+System.Collections.Hashtable whereClauses = new System.Collections.Hashtable(1);
+whereClauses.Add("Customer", whereClause);
 
+
+// Create SearchOptions
+SearchOptions searchOptions = SearchOptions.CreateRuntimeSearch(whereClauses, DataSetMode.RowsDataSet);
+
+
+/****************OPTIONS****************/
 // Invoke Search with Hashtable Where clauses
-    SearchOptions searchOptions = SearchOptions.CreateRuntimeSearch(whereClauses, DataSetMode.RowsDataSet);
-    this.custAdapter.InvokeSearch(searchOptions);
+this.custAdapter.InvokeSearch(searchOptions);
+
+    or
+
+// Adapter Method
+bool morePages;
+DataSet dsUD100 = ud100Adapter.GetRows(searchOptions, out morePages);
+/********************************************/
+
+
     
-    
-    
+/****************OPTIONS****************/
 // Get Data Row
     DataRow dataRow = this.adapterName.TableNameData.TableName.Rows[row index];
     dataRow["Column Name"]
-    
-// Select Data Row
+   
+    or
+   
+// SELECT Data Row
     DataRow[] copyRows = ud100Adapter.UD100Data.UD100A.Select("ChildKey1 = \'" + txtCopyPart.Text.ToString() + "\' and ChildKey2 = \'" + txtCopyRev.Text.ToString() + "\'");
-					
+/********************************************/
 
                     
                     
 
-/********** BO Adapter  - Invoke Search **********/
+                    
+                    
+                    
+/********** BO Adapter  - GetByID Search **********/
 // Set BO adapter
 this.partRevAdapter = new PartRevSearchAdapter(this.oTrans);
 this.partRevAdapter.BOConnect();
@@ -38,12 +57,13 @@ this.partRevAdapter.GetByID(txtCopyPart.Text, txtCopyRev.Text, "");
 
 // Get Row
 DataRow dataRow = this.partRevAdapter.PartRevSearchData.PartRev.Rows[0];
-    
-    
-    
-    
-    
-    
+
+                    
+                    
+
+                    
+                    
+                
 /********** Simple Search Function List Lookup **********/
     bool recSelected;
     string whereClause = string.Empty;
