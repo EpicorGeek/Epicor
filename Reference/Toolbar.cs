b5e41fc911287b708a899cc4fd9067f8b6ff8f1f
Evan.Mateo <gevanmateo@gmail.com>
2018-01-30 09:24:42 -0500

    Toolbar: add more cust

--- Reference/Toolbar.cs
+++ Reference/Toolbar.cs
@@ -7,3 +7,34 @@ foreach (ToolBase tool in baseToolbarsManager.Tools)
         tool.SharedProps.Visible = false; 
     }
 }
+
+
+/* All Toolsbars (Toolbase) */
+try
+{
+	 string message = "";
+	 foreach (UltraToolbar toolbar in baseToolbarsManager.Toolbars)
+{
+	// add the toolbar name
+	message+="Tool Key: "+tool.Key+ "\tTool Type: "+tool.GetType().Name+"\n";
+}
+MessageBox.Show(message);
+} catch (Exception e)
+{
+	ExceptionBox.Show(e);
+}
+
+/* All Tools (Toolbase) */
+try
+{
+	 string message = "";
+	 foreach (ToolBase tool in baseToolbarsManager.Tools)
+{
+	// add the toolbar name
+	message+="Tool Key: "+tool.Key+ "\tTool Type: "+tool.GetType().Name+"\n";
+}
+MessageBox.Show(message);
+} catch (Exception e)
+{
+	ExceptionBox.Show(e);
+}
